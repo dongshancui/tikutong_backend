8d7fbd4e54876541c2322404ab347b9cf1117719
崔东山 <13437678+dongshancui@user.noreply.gitee.com>
2024-12-15 19:59:27 +0800

    version1 1. 补充question对应代码
    Signed-off-by: 崔东山 <13437678+dongshancui@user.noreply.gitee.com>

---
+++ src/main/java/com/dongshan/tikutong/model/dto/questionBank/QuestionBankAddRequest.java
@@ -0,0 +1,43 @@
+package com.dongshan.tikutong.model.dto.questionBank;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 创建题库请求
+ *
+ * @author <a href="https://github.com/lidongshan">程序员鱼皮</a>
+ * @from <a href="https://www.code-nav.cn">编程导航学习圈</a>
+ */
+@Data
+public class QuestionBankAddRequest implements Serializable {
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 内容
+     */
+    private String content;
+
+    /**
+     * 描述
+     */
+    private String description;
+
+    /**
+     * 图片
+     */
+    private String picture;
+
+    /**
+     * 创建用户id
+     */
+    private Long userId;
+
+    private static final long serialVersionUID = 1L;
+}
