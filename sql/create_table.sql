296797957c1d4140eed759ca1c0a71b5a7c5e097
dongshanta <dongshanta@163.com>
2024-12-28 10:40:26 +0800

    #fix bug  - 修复数据库表-题目题库关系表中的editTime字段缺失问题

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -65,6 +65,7 @@ create table if not exists question_bank_question
     questionBankId bigint                             not null comment '题库 id',
     questionId     bigint                             not null comment '题目 id',
     userId         bigint                             not null comment '创建用户 id',
+    editTime     datetime default CURRENT_TIMESTAMP not null comment '创建时间',
     createTime     datetime default CURRENT_TIMESTAMP not null comment '创建时间',
     updateTime     datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     UNIQUE (questionBankId, questionId)
